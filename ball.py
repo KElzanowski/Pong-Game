ae389b240be073b79ae2ae98e5ca6b4c627cdc5f
KelzaPrograms <79310242+KelzaPrograms@users.noreply.github.com>
2021-05-25 17:19:56 +0100

    Create ball.py

---
+++ ball.py
@@ -0,0 +1,31 @@
+from turtle import Turtle
+
+
+class Ball(Turtle):
+    def __init__(self):
+        super().__init__()
+        self.shape("circle")
+        self.color("white")
+        self.penup()
+        self.x_move = 15
+        self.y_move = 15
+        self.move_speed = 0.1
+
+    def move(self):
+        new_x = self.xcor() + self.x_move
+        new_y = self.ycor() + self.y_move
+        self.goto(new_x, new_y)
+        """Makes ball move 15 pixels right and up at the start of the game, at every screen refresh, until ball
+        bounces."""
+
+    def bounce_y(self):
+        self.y_move *= -1
+
+    def bounce_x(self):
+        self.x_move *= -1
+        self.move_speed *= 0.9
+
+    def reset_position(self):
+        self.goto(0, 0)
+        self.move_speed = 0.1
+        self.bounce_x()
